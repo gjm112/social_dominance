022602256f28006af93efdd390694cfd4fba7483
Nora Lee Reinhardt <nreinhardt@luc.edu>
2025-04-01 15:24:58 -0500

    finished the graphs

--- code/code.R
+++ code/code.R
@@ -87,6 +87,322 @@ boxes1 %>% filter(grepl('transwomen', group)) %>%
   geom_bar(position = "fill", stat = "identity") +
   theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
 
+# nonbinary boxplots
+boxes1 %>% filter(grepl('nonbinary', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# bisexual boxplots
+boxes1 %>% filter(grepl('bisexual', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# gay boxplots
+boxes1 %>% filter(grepl('gay', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# black boxplots
+boxes1 %>% filter(grepl('black', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# asian boxplots
+boxes1 %>% filter(grepl('asian', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# undocumented boxplots
+boxes1 %>% filter(grepl('undocumented', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# midage boxplots
+boxes1 %>% filter(grepl('midage', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# addict boxplots
+boxes1 %>% filter(grepl('addict', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# anxiety boxplots
+boxes1 %>% filter(grepl('anxiety', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# ocd boxplots
+boxes1 %>% filter(grepl('ocd', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# bipolar boxplots
+boxes1 %>% filter(grepl('bipolar', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# intellectual boxplots
+boxes1 %>% filter(grepl('intellectual', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# criminal boxplots
+boxes1 %>% filter(grepl('criminals', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# facialdeform boxplots
+boxes1 %>% filter(grepl('facialdeform', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# prosthetic boxplots
+boxes1 %>% filter(grepl('prosthetics', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# nondisabled boxplots
+boxes1 %>% filter(grepl('nondisabled', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# deaf boxplots
+boxes1 %>% filter(grepl('deaf', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# diabetes boxplots
+boxes1 %>% filter(grepl('diabetes', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# autism boxplots
+boxes1 %>% filter(grepl('autism', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# cancer boxplots
+boxes1 %>% filter(grepl('cancer', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# muscdystrophy boxplots
+boxes1 %>% filter(grepl('muscdystrophy', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# ciswomen boxplots
+boxes1 %>% filter(grepl('ciswomen', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+
+# transmen boxplots
+boxes1 %>% filter(grepl('transmen', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+
+# heterosexual boxplots
+boxes1 %>% filter(grepl('heterosexual', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+
+# lesbian boxplots
+boxes1 %>% filter(grepl('lesbian', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# white boxplots
+boxes1 %>% filter(grepl('white', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# latinxhispanic boxplots
+boxes1 %>% filter(grepl('latinxhispanic', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# immigrants boxplots
+boxes1 %>% filter(grepl('immigrants', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# elderly boxplots
+boxes1 %>% filter(grepl('elderly', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# youngadults boxplots
+boxes1 %>% filter(grepl('youngadults', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+
+# schizophrenia boxplots
+boxes1 %>% filter(grepl('schizophrenia', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# homeless boxplots
+boxes1 %>% filter(grepl('homeless', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# depression boxplots
+boxes1 %>% filter(grepl('depression', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+
+# Alzheimer boxplots
+boxes1 %>% filter(grepl('alzheimer', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+
+# sociopaths boxplots
+boxes1 %>% filter(grepl('sociopaths', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+
+# amputees boxplots
+boxes1 %>% filter(grepl('amputees', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# hivaids boxplots
+boxes1 %>% filter(grepl('hivaids', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+
+# wheelchair boxplots
+boxes1 %>% filter(grepl('wheelchair', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# cochlearimplants boxplots
+boxes1 %>% filter(grepl('cochlearimplants', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# blind boxplots
+boxes1 %>% filter(grepl('blind', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# autoimmune boxplots
+boxes1 %>% filter(grepl('autoimmune', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# adhd boxplots
+boxes1 %>% filter(grepl('adhd', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+
+# epilepsy boxplots
+boxes1 %>% filter(grepl('epilepsy', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
+
+# crohns boxplots
+boxes1 %>% filter(grepl('crohns', group)) %>% 
+  ggplot(aes(x = group, y = count, fill = as.factor(score))) + 
+  geom_bar(position = "fill", stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, margin = margin(t = 15)))
+
 # i want to loop through all groups but not working rn
 for (g in length(groups)){
   boxes %>% filter(grepl(groups[g], group)) %>% ggplot(aes(y = score)) + geom_boxplot() + facet_wrap(~ group, ncol =4)
